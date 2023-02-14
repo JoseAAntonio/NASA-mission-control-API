45737565ca01ac99ee8361f32bace3293526d7d1
Jose Antonio <antoniojaraujo0@gmail.com>
2023-02-14 17:14:24 -0300

    serving react frontEnd in production adding deploy scrip on global package.json

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 
 
@@ -9,7 +10,12 @@ const app = express();
 app.use(cors({origin: 'http://localhost:3000',}));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use(planetsRouter)
+app.get('/', (req, res) => {
+    res.send(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 
 
