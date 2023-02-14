862d39161abde9bfb5efc538bbe9eb7ebfa2ef61
Jose Antonio <antoniojaraujo0@gmail.com>
2023-02-13 23:01:16 -0300

    add global package.json for running both server and client at the same time

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,6 +1,7 @@
 
 const { parse } = require("csv-parse");
 const fs = require("fs");
+const path = require("path");
 
 const habitablePlanets = [];
 
@@ -13,28 +14,45 @@ const isHabitable = (planet) => {
 	);
 };
 
-fs.createReadStream("kepler_data.csv")
-	.pipe(
-		parse({
-			comment: "#",
-			columns: true,
-		})
-	)
-	.on("data", (data) => {
-		isHabitable(data) ? habitablePlanets.push(data) : null;
-	})
-	.on("error", (err) => {
-		console.log(err);
-	})
-	.on("end", () => {
-		console.log(
-			habitablePlanets.map((planet) => {
-				return planet["kepler_name"];
-			})
-		);
-		console.log(`${habitablePlanets.length} habitable planets`);
-	});
+//NOTE - Our planets data is loaded and parsed as a STREAM heppening asynchronosly, so our module.exports doenst wait around for that to finish and sends the data without it been set yet, for that we need to encapsulate this process in a promisse, exporting it and only start listening on our server when the data is readyfor incoming requests.
+/*
+const promise = new Promise((resolve, reject) => {
+    resolve(42)
+})
+promise.then((result) => {
+
+})
+const result = await promise;
+console.log(result);
+*/
+
+// so the function loadPlanetsData return a promise which resolves when our habitablePlanets heve been found
+
+function loadPlanetsData () {
+    return new Promise((resolve, reject) => {
+            fs.createReadStream(path.join(__dirname, "..", "..", "data", "kepler_data.csv"))
+        .pipe(
+            parse({
+                comment: "#",
+                columns: true,
+            })
+        )
+        .on("data", (data) => {
+            isHabitable(data) ? habitablePlanets.push(data) : null;
+        })
+        .on("error", (err) => {
+            console.log(err);
+            reject(err)
+        })
+        .on("end", () => {
+            console.log(`${habitablePlanets.length} habitable planets`);
+            resolve()
+        }); 
+    });
+}
+
 
     module.exports = {
+        loadPlanetsData,
         planets: habitablePlanets,
     }
--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,28 @@ const http = require('http')
 
 const app = require('./app')
 
+const { loadPlanetsData } = require('./models/planets.model')
+
 const PORT = process.env.PORT || 8000;
 
 //NOTE using express as a fancy listener function for our built-in node http server. now we can orginaze our code a little bit more by moving our express code to another file (app.js)
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}...`);
-})
+
+//NOTE - starting the server only when our asynchronous data is ready for incoming request from the cliente
+async function startServer () {
+    try {
+        await loadPlanetsData()
+        server.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}...`);
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+startServer()
+
 
 
 
